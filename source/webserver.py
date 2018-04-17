f9c3acafde32b82b540840f865e2793aeb8afa7f
dwainetrain <dwaine.best@gmail.com>
2018-04-17 05:54:34 -0700

    Add db connection to webserver, add endpoint query at restaurants.

---
+++ source/webserver.py
@@ -0,0 +1,96 @@
+import cgi
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+class WebServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print (output)
+                return
+            
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>&#16Hola<a href= '/hello'>Back to Hello</a></body></html>"
+                self.wfile.write(output)
+                print (output)
+                return
+
+            if self.path.endswith("/restaurants"):
+                engine = create_engine('sqlite:///restaurantmenu.db')
+                Base.metadata.bind = engine
+                DBSession = sessionmaker(bind = engine)
+                session = DBSession()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                query = session.query(Restaurant.name).all()
+                output = ""
+                output += "<html><body><ul>" 
+                for entry in query:
+                    output += "<li> %s </li>" % entry
+                output += "</body></html>"
+                self.wfile.write(output)
+                print (output)
+                return
+
+
+
+        except IOError:
+            self.send_error(404, "File Not Found %s" % self.path)
+    
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print(output)
+
+        except:
+            self.send_error(404, "File Not Found %s" % self.path)
+
+
+def main():
+    try:
+        port = 7800
+        server = HTTPServer(('',port), WebServerHandler)
+        print ("Web server running on http://localhost:%s" % port)
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print ("^C entered, stopping web server...")
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
